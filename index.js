72f2e7897d69cdbb46e8707923d5a0703add39ce
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-07-30 18:19:48 -0300

    responsivo aplicado

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function changeCircleColor(color) {
     const circle = document.querySelector('.circle')
     circle.style.background = color
 }
+
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle')
+    menuToggle.classList.toggle('active')
+    
+    var navegation = document.querySelector('.navegation')
+    navegation.classList.toggle('active')
+}
